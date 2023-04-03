9a6a126f8ff58f40b5b951228ca79d72e170c929
Claudia Afonso <claudia1087@gmail.com>
2023-04-04 00:43:02 +0200

    adds more styles to cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,18 +6,86 @@ type CardProps = {
   character: Character
 }
 
-export const Card: React.FC<CardProps> = ({character}) => {
+export const Card: React.FC<CardProps> = ({ character }) => {
+  console.log(character)
   return (
     <div className={style.card}>
-        <p><span>name: </span><span>{character.name}</span></p>
-        <p><span>status: </span><span>{character.status}</span></p>
-        <p><span>species: </span><span>{character.species}</span></p>
-        <p><span>type: </span><span>{character.type}</span></p>
-        <p><span>gender: </span><span>{character.gender}</span></p>
-        {/* <p><span>origin: </span><span>{character.origin}</span></p>
-        <p><span>location: </span><span>{character.location}</span></p> */}
-        <p><span>image: </span><span>{character.image}</span></p>
+      <div className={style.details}>
+        <p className={style.detailsText}>
+          <span className={style.detailsTitle}>Name: </span>
+          <span className={style.detailsContent}>{character.name}</span>
+        </p>
+        <p className={style.detailsText}>
+          <span className={style.detailsTitle}>Status: </span>
+          <span className={style.detailsContent}>{character.status}</span>
+        </p>
+        <p className={style.detailsText}>
+          <span className={style.detailsTitle}>Species: </span>
+          <span className={style.detailsContent}>{character.species}</span>
+        </p>
+        <p className={style.detailsText}>
+          <span className={style.detailsTitle}>Type: </span>
+          <span className={style.detailsContent}>{character.type}</span>
+        </p>
+        <p className={style.detailsText}>
+          <span className={style.detailsTitle}>Gender: </span>
+          <span className={style.detailsContent}>{character.gender}</span>
+        </p>
+        {character.origin &&
+          <>
+            <p className={style.detailsText}>
+              <span className={style.detailsTitle}>Origin: </span>
+            </p>
+            <p className={`${style.detailsText} ${style.subDetails}`}>
+              <span className={style.detailsTitle}>Name: </span>
+              <span className={style.detailsContent}>{character.origin.name}</span>
+            </p>
+            <p className={`${style.detailsText} ${style.subDetails}`}>
+              <span className={style.detailsTitle}>Dimension: </span>
+              <span className={style.detailsContent}>{character.origin.dimension}</span>
+            </p>
+            <p className={`${style.detailsText} ${style.subDetails}`}>
+              <span className={style.detailsTitle}>Type: </span>
+              <span className={style.detailsContent}>{character.origin.type}</span>
+            </p>
+            <p className={`${style.detailsText} ${style.subDetails}`}>
+              <span className={style.detailsTitle}>Name: </span>
+              <span className={style.detailsContent}>{character.origin.residents.length}</span>
+            </p>
+          </>
+        }
+        {character.location &&
+          <>
+            <p className={style.detailsText}>
+              <span className={style.detailsTitle}>Location: </span>
+            </p>
+            <p className={`${style.detailsText} ${style.subDetails}`}>
+              <span className={style.detailsTitle}>Name: </span>
+              <span className={style.detailsContent}>{character.location.name}</span>
+            </p>
+            <p className={`${style.detailsText} ${style.subDetails}`}>
+              <span className={style.detailsTitle}>Dimension: </span>
+              <span className={style.detailsContent}>{character.location.dimension}</span>
+            </p>
+            <p className={`${style.detailsText} ${style.subDetails}`}>
+              <span className={style.detailsTitle}>Type: </span>
+              <span className={style.detailsContent}>{character.location.type}</span>
+            </p>
+            <p className={`${style.detailsText} ${style.subDetails}`}>
+              <span className={style.detailsTitle}>Residents: </span>
+              <span className={style.detailsContent}>{character.location.residents.length}</span>
+            </p>
+          </>
+        }
         {/* <p><span>episode: </span><span>{character.episode}</span></p> */}
+      </div>
+      <div className={style.imgContainer}>
+        <img
+          src={character.image || ''}
+          alt={character.name || ''}
+          className={style.img}
+        />
+      </div>
     </div>
   );
 }
